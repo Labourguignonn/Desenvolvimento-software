f7fe85b419e3b3fe6f5c1c6b878acee6c0396e0c
luis-erf <luisedu2395@gmail.com>
2024-12-08 16:10:00 -0300

    atulização do código com comentários e funções

--- api_banco_de_dados.py
+++ api_banco_de_dados.py
@@ -1,14 +1,40 @@
 import requests
 import re
 
+#Variáveis
+MaxRuntime = int(input())
+chosenGenre = input()
+chosenRating = input()
+
+def getTitle(indice):
+    #definir como vai ser a chamada de cada titulo
+    return base["title"][indice]
+
+def getOverview(indice):
+    return base["overview"][indice]
+
+def getRuntime(indice):
+    return base["runtime"][indice]
+
+def getPoster(indice):
+    return base["poster_path"][indice]
+    
+def getDiretor(indice):
+    return base["Diretor"][indice]
+    
 headers = {
     "accept": "application/json",
     "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3YjJlZjM0MWRhOWM1NTM3ZThmOGRlNzA0ZDJhM2M2ZiIsIm5iZiI6MTczMzQyNzUyMS44NjcsInN1YiI6IjY3NTIwMTQxYWIzN2ZjZDNlODg1MTMzMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.iVq091IX6ZNnQeht3EhzNOotU61iFU8mcyFq7a2INQQ"
 }
 
-films_lists = ['onde os fracos nao tem vez']
-base = {"title" : [], "overview" : [], 'runtime' : [], 'Classificação indicativa' : [], 'poster_path' : [], "Diretor" : []}
+films_lists = ['A Culpa é das Estrelas', ' Diário de uma Paixão', ' O Diário da Princesa', ' A Barraca do Beijo', ' Para Todos os Garotos que Já Amei']
+right_films_list = []
+#local onde ficam armazenados os dados dos filmes
+base = {"title" : [], "overview" : [], 'runtime' : [], 'poster_path' : [], "Diretor" : []}
 id_filmes = []
+
+
+#Pegar o id de cada filme
 for i in range(len(films_lists)):
     films_lists[i].lower()
     search = re.sub(' ', '%20', films_lists[i])
@@ -16,21 +42,28 @@ for i in range(len(films_lists)):
     chamada = requests.get(url, headers=headers)
     filme = chamada.json()["results"][0]
     id_filmes.append(filme["id"])
+
+
+#Pegar os dados de cada filme
 for i in range(len(films_lists)):
     url_id = f"https://api.themoviedb.org/3/movie/{id_filmes[i]}?language=pt-BR"
     chamada = requests.get(url_id, headers=headers)
-    base["title"].append(chamada.json()["title"])
-    base["overview"].append(chamada.json()["overview"])
-    base["runtime"].append(chamada.json()["runtime"])
-    base["poster_path"].append(chamada.json()["poster_path"])
-    diretor = False
-    url_diretor = f"https://api.themoviedb.org/3/movie/{id_filmes[i]}/credits?language=en-US"
-    chamada_casting = requests.get(url_diretor, headers=headers)
-    iterar = chamada_casting.json()["crew"]
-    base["Diretor"].append([])
-    for j in range(len(iterar)):
-        if(iterar[j]["job"] == "Director"):
-            base["Diretor"][i].append(iterar[j]["name"])
-
+    if chamada.json()["runtime"] <= MaxRuntime + 10:
+        right_films_list.append(films_lists[i])
+        
+        ##adicionando no dicionário
+        base["title"].append(chamada.json()["title"])
+        base["overview"].append(chamada.json()["overview"])
+        base["runtime"].append(chamada.json()["runtime"])
+        base["poster_path"].append(chamada.json()["poster_path"])
+        
+        #Achar o diretor
+        url_diretor = f"https://api.themoviedb.org/3/movie/{id_filmes[i]}/credits?language=en-US"
+        chamada_casting = requests.get(url_diretor, headers=headers)
+        iterar = chamada_casting.json()["crew"]
+        base["Diretor"].append([])
+        for j in range(len(iterar)):
+            if(iterar[j]["job"] == "Director"):
+                base["Diretor"][len(right_films_list) - 1].append(iterar[j]["name"])
 
 
