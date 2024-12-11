97f0fead495f8e2429ecd0884522ad323f958eb3
Giovanna Mafra <104537611+GiovannaMafra@users.noreply.github.com>
2024-12-11 16:20:11 -0300

    ajustes de cor

--- src/pages/Home/Filtroidade.jsx
+++ src/pages/Home/Filtroidade.jsx
@@ -12,27 +12,27 @@ function Filtroidade(){
             </div>
                 <div class="container">
         <div class="row">
-            <button class="button-option">L</button>
-            <button class="button-option">Classificação Livre</button>
+            <button class="button-option-livre">L</button>
+            <button class="button-option">Livre para todos os públicos</button>
         </div>
         <div class="row">
-            <button class="button-option">10</button>
+            <button class="button-option-10">10</button>
             <button class="button-option">Não recomendado p/ menores de 10 anos</button>
         </div>
         <div class="row">
-            <button class="button-option">12</button>
+            <button class="button-option-12">12</button>
             <button class="button-option">Não recomendado p/ menores de 12 anos</button>
         </div>
         <div class="row">
-            <button class="button-option">14</button>
+            <button class="button-option-14">14</button>
             <button class="button-option">Não recomendado p/ menores de 14 anos</button>
         </div>
         <div class="row">
-            <button class="button-option">16</button>
+            <button class="button-option-16">16</button>
             <button class="button-option">Não recomendado p/ menores de 16 anos</button>
         </div>
         <div class="row">
-            <button class="button-option">18</button>
+            <button class="button-option-18">18</button>
             <button class="button-option">Não recomendado p/ menores de 18 anos</button>
         </div>
     </div>
---
+++ src/pages/Home/carregamento.jsx
@@ -0,0 +1,18 @@
+import './stylecarregamento.css'
+
+function Carregamento(){
+
+    return (
+        <div class="central">
+            <div>
+            <img src="assets/loader.gif" alt="Descrição do GIF" class="gif-style"/>
+            <div class="titulo">
+                <h1>Carregando seus filmes...</h1>
+            </div>
+            </div>
+        </div>
+    )
+
+}
+
+export default Carregamento
