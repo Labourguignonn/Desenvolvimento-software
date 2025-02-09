651c240383598b7fed3f61158ff65239e41d0a30
Lucas Araujo Bourguignon <labourguginon@gmail.com>
2025-02-09 12:05:00 -0300

    creating public subdiretory

--- Frontend/app/vite.config.js
+++ Frontend/app/vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Garante que os caminhos relativos funcionarão corretamente
+  base: './', // Garante caminhos relativos para Netlify
+  build: {
+    outDir: 'dist',
+  },
+  publicDir: resolve(__dirname, 'public'), // Garante que `public/` seja copiado para `dist/`
 });
