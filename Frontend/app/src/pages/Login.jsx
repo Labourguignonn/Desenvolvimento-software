97c293df7fe12fc8d30eccdcf876cb5353a1d797
Lima Araújo <158223826+limaraujo@users.noreply.github.com>
2025-02-17 17:40:34 -0300

    Update Login.jsx

--- Frontend/app/src/pages/Login.jsx
+++ Frontend/app/src/pages/Login.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import "../styles/Login.css";
 import { baseURL } from "../services/config";
+import eyeIcon from "../assets/eye-icon.svg";
 
 function Login() {
   const navigate = useNavigate();
@@ -11,7 +12,7 @@ function Login() {
   const [confirmPassword, setConfirmPassword] = useState(""); // Estado para confirmar senha
   const [text_user, setTextUser] = useState("");
   const [isRegistering, setIsRegistering] = useState(false);
-  
+
   // Estados para controle de visibilidade de senha
   const [showPassword, setShowPassword] = useState(false);
   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
@@ -76,39 +77,39 @@ function Login() {
       alert("Existem campos sem nada digitado no login. Favor completar todos os campos!");
       return;
     }
-  
+
     try {
       // Verifica se o usuário e a senha estão corretos
       const authResponse = await axios.post(`${baseURL}/verificar-login`, {
         username: text_user,
         password: text_password,
       });
-  
+
       if (!authResponse.data.success) {
         alert("Usuário ou senha incorretos!");
         return;
       }
-  
+
       // Se as credenciais estiverem corretas, envia a chave
       const keyResponse = await axios.post(`${baseURL}/receber_chave`, {
         user: text_user,
         password: text_password,
         key: text_key,
       });
-  
+
       if (keyResponse.data.chave) {
         console.log("Login bem-sucedido:", keyResponse.data);
         navigate("/escolha_generos");
       } else {
         alert("Erro ao validar a chave.");
       }
-      
+
     } catch (error) {
       console.error("Erro ao enviar dados:", error);
       alert("Erro ao enviar dados.");
     }
   };
-  
+
 
   // Função para alternar a visibilidade da senha
   const togglePasswordVisibility = () => setShowPassword(!showPassword);
@@ -116,107 +117,70 @@ function Login() {
   const toggleKeyVisibility = () => setShowKey(!showKey);
 
   return (
+    // Formulário de registro
     <div className="container_login">
       {isRegistering ? (
         <div className="container_register">
           <h2>Registre-se</h2>
-          <div className="container_title_input">
-            <p className="mt-2">Usuário</p>
-            <input
-              type="text"
-              placeholder="Digite seu usuário..."
-              value={text_user}
-              onChange={(e) => setTextUser(e.target.value)}
-              className="input_box"
-            />
-          </div>
-          <div className="container_title_input">
-            <p className="mt-2">Criar Senha</p>
-            <input
-              type={showPassword ? "text" : "password"}
-              placeholder="Digite sua senha..."
-              value={text_password}
-              onChange={(e) => setTextPassword(e.target.value)}
-              className="input_box"
-            />
-            <button type="button" onClick={togglePasswordVisibility}>
-              {showPassword ? "Ocultar" : "Mostrar"}
-            </button>
-          </div>
-          <div className="container_title_input">
-            <p className="mt-2">Confirmar Senha</p>
-            <input
-              type={showConfirmPassword ? "text" : "password"}
-              placeholder="Confirme sua senha..."
-              value={confirmPassword}
-              onChange={(e) => setConfirmPassword(e.target.value)}
-              className="input_box"
-            />
-            <button type="button" onClick={toggleConfirmPasswordVisibility}>
-              {showConfirmPassword ? "Ocultar" : "Mostrar"}
-            </button>
-          </div>
-          <button className="button_config" onClick={registrarUsuario}>
-            Registrar
-          </button>
-          <p
-            onClick={() => setIsRegistering(false)}
-            className="toggle_form"
-            style={{ cursor: "pointer" }}
-          >
+
+          {['Usuário', 'Criar Senha', 'Confirmar Senha'].map((label, index) => (
+            <div className="container_title_input" key={index}>
+              <p className="mt-2">{label}</p>
+              <div className="field_with_eye">
+                <input
+                  type={label === 'Usuário' ? "text" : (label === 'Criar Senha' && showPassword) || (label === 'Confirmar Senha' && showConfirmPassword) ? "text" : "password"}
+                  placeholder={label === 'Usuário' ? "Digite seu usuário..." : `Digite sua ${label.toLowerCase()}...`}
+                  value={label === 'Usuário' ? text_user : label === 'Criar Senha' ? text_password : confirmPassword}
+                  onChange={(e) => label === 'Usuário' ? setTextUser(e.target.value) : label === 'Criar Senha' ? setTextPassword(e.target.value) : setConfirmPassword(e.target.value)}
+                  className="input_box"
+                />
+                {(label === 'Criar Senha' || label === 'Confirmar Senha') && (
+                  <button type="button" className="eye-icon" onClick={label === 'Criar Senha' ? togglePasswordVisibility : toggleConfirmPasswordVisibility}>
+                    <img src={eyeIcon} alt="Ícone de olho" />
+                  </button>
+                )}
+              </div>
+            </div>
+          ))}
+
+          <button className="button_config" onClick={registrarUsuario}>Registrar</button>
+          <p onClick={() => setIsRegistering(false)} className="toggle_form" style={{ cursor: "pointer" }}>
             Já tem uma conta? Faça login
           </p>
         </div>
+
       ) : (
+
+        // Formulário de login
         <div className="container_informations">
           <h2>Login</h2>
-          <div className="container_title_input">
-            <p className="mt-2">Usuário</p>
-            <input
-              type="text"
-              placeholder="Digite seu usuário..."
-              value={text_user}
-              onChange={(e) => setTextUser(e.target.value)}
-              className="input_box"
-            />
-          </div>
-          <div className="container_title_input">
-            <p className="mt-2">Senha</p>
-            <input
-              type={showPassword ? "text" : "password"}
-              placeholder="Digite sua senha..."
-              value={text_password}
-              onChange={(e) => setTextPassword(e.target.value)}
-              className="input_box"
-            />
-            <button type="button" onClick={togglePasswordVisibility}>
-              {showPassword ? "Ocultar" : "Mostrar"}
-            </button>
-          </div>
-          <div className="container_title_input">
-            <p className="mt-2">Senha OPEN AI</p>
-            <input
-              type={showKey ? "text" : "password"}
-              placeholder="Digite sua senha da API..."
-              value={text_key}
-              onChange={(e) => setTextKey(e.target.value)}
-              className="input_box"
-            />
-            <button type="button" onClick={toggleKeyVisibility}>
-              {showKey ? "Ocultar" : "Mostrar"}
-            </button>
-          </div>
-          <button className="button_config" onClick={loginUsuario}>
-            Enviar
-          </button>
-          <p
-            onClick={() => setIsRegistering(true)}
-            className="toggle_form"
-            style={{ cursor: "pointer" }}
-          >
+          {['Usuário', 'Senha', 'Senha OPEN AI'].map((label, index) => (
+            <div className="container_title_input" key={index}>
+              <p className="mt-2">{label}</p>
+              <div className="field_with_eye">
+                <input
+                  type={label === 'Usuário' ? "text" : (label === 'Senha' && showPassword) || (label === 'Senha OPEN AI' && showKey) ? "text" : "password"}
+                  placeholder={`Digite sua ${label.toLowerCase()}...`}
+                  value={label === 'Usuário' ? text_user : label === 'Senha' ? text_password : text_key}
+                  onChange={(e) => label === 'Usuário' ? setTextUser(e.target.value) : label === 'Senha' ? setTextPassword(e.target.value) : setTextKey(e.target.value)}
+                  className="input_box"
+                />
+                {(label === 'Senha' || label === 'Senha OPEN AI') && (
+                  <button type="button" className="eye-icon" onClick={label === 'Senha' ? togglePasswordVisibility : toggleKeyVisibility}>
+                    <img src={eyeIcon} alt="Ícone de olho" />
+                  </button>
+                )}
+              </div>
+            </div>
+          ))}
+
+          <button className="button_config" onClick={loginUsuario}>Enviar</button>
+          <p onClick={() => setIsRegistering(true)} className="toggle_form" style={{ cursor: "pointer" }}>
             Não tem uma conta? Registre-se
           </p>
         </div>
+
+
       )}
     </div>
   );
