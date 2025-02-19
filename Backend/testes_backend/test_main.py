1c4e132b3ede3d64ab96233ef862596e9864667d
Luis <luisedu2395@gmail.com>
2025-02-19 20:21:16 -0300

    adição de alguns testes a mais na main

--- Backend/testes_backend/test_main.py
+++ Backend/testes_backend/test_main.py
@@ -38,3 +38,69 @@ def test_getSelectedRating_failure(client):
     # Verificar o conteúdo da resposta
     json_data = response.get_json()
     assert json_data["error"] == "Classificação não enviada!"
+
+def test_getSelectedGenres_success(client):
+    # Dados de entrada (simulando o JSON que será enviado)
+    data = {"selectedGenres": ["Action", "Comedy"]}
+
+    # Enviar a requisição POST
+    response = client.post("/selecionar_generos", json=data)
+
+    # Verificar se o status da resposta é 200
+    assert response.status_code == 200
+    # Verificar o conteúdo da resposta
+    json_data = response.get_json()
+    assert json_data["message"] == "Gêneros recebidos com sucesso"
+    assert json_data["selectedGenres"] == ["Action", "Comedy"]
+
+def test_getSelectedGenres_failure(client):
+    # Enviar a requisição POST com um JSON vazio (sem 'selectedGenres')
+    data = {}
+
+    response = client.post("/selecionar_generos", json=data)
+
+    # Verificar se o status da resposta é 400
+    assert response.status_code == 400
+    # Verificar o conteúdo da resposta
+    json_data = response.get_json()
+    assert json_data["error"] == "Selecione de 1 a 3 gêneros"
+
+def test_getSelectedRuntime_success(client):
+    # Dados de entrada (simulando o JSON que será enviado)
+    data = {"time": 120}
+
+    # Enviar a requisição POST
+    response = client.post("/tempo", json=data)
+
+    # Verificar se o status da resposta é 200
+    assert response.status_code == 200
+    # Verificar o conteúdo da resposta
+    json_data = response.get_json()
+    assert json_data["message"] == "Tempo recebida com sucesso!"
+    assert json_data["time"] == 120
+
+def test_getSelectedRuntime_failure(client):
+    # Enviar a requisição POST com um JSON vazio (sem 'time')
+    data = {}
+
+    response = client.post("/tempo", json=data)
+
+    # Verificar se o status da resposta é 400
+    assert response.status_code == 400
+    # Verificar o conteúdo da resposta
+    json_data = response.get_json()
+    assert json_data["error"] == "Escolha uma duração máxima para seu filme"
+
+def test_getApiKey_success(client):
+    # Dados de entrada (simulando o JSON que será enviado)
+    data = {"key": "fake-key"}
+
+    # Enviar a requisição POST
+    response = client.post("/receber_chave", json=data)
+
+    # Verificar se o status da resposta é 200
+    assert response.status_code == 200
+    # Verificar o conteúdo da resposta
+    json_data = response.get_json()
+    assert json_data["message"] == "Chave recebido com sucesso!"
+    assert json_data["chave"] == "fake-key"
