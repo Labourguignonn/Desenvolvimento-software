79e667ad9c7fd537b065fca334df40daf9e58847
Lucas Araujo Bourguignon <107072168+Labourguignonn@users.noreply.github.com>
2025-02-28 15:19:50 -0300

    rating and runtime clean

---
+++ Frontend/app/src/components/ErrorMessage.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import '../styles/ErrorMessage.css'; // Importando o arquivo CSS
+
+const ErrorMessage = ({ message, onClose }) => {
+  return (
+    <div className="error-message">
+      <button className="refazer-button" onClick={onClose}>
+        <div className="error">
+          <div className="error__close">
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20">
+              <path fill="#393a37" d="M15.8333 5.34166l-1.175-1.175-4.6583 4.65834-4.65833-4.65834-1.175 1.175 4.65833 4.65834-4.65833 4.6583 1.175 1.175 4.65833-4.6583 4.6583 4.6583 1.175-1.175-4.6583-4.6583z"/>
+            </svg>
+          </div>
+          <div className="message-error">
+            <div className="error__icon">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                <path fill="#393a37" d="M13 13h-2v-6h2zm0 4h-2v-2h2zm-1-15c-1.3132 0-2.61358.25866-3.82683.7612..."/>
+              </svg>
+            </div>
+            <div className="error__title">{message}</div>
+          </div>
+        </div>
+      </button>
+    </div>
+  );
+};
+
+export default ErrorMessage;
--- Frontend/app/src/pages/RuntimeSelection.jsx
+++ Frontend/app/src/pages/RuntimeSelection.jsx
@@ -2,11 +2,15 @@ import React, { useState } from "react";
 import { useNavigate } from 'react-router-dom';
 import axios from "axios";
 import "../styles/RuntimeSelection.css";
+import BackButton from "../components/BackButton";
+import NextButton from "../components/NextButton";
 import { baseURL } from "../services/config";
+
+
 
 const RuntimeSelection = () => {
   const navigate = useNavigate();
-
+  
   const times = [
     { label: "1h30", value: "90" },
     { label: "1h45", value: "105" },
@@ -15,9 +19,10 @@ const RuntimeSelection = () => {
     { label: "3h00", value: "180" },
     { label: "Longo", value: "210" },
   ];
-
+  
   const [selectedTime, setSelectedTime] = useState(times[0].value);
 
+  // Função para selecionar o tempo
   const handleTimeClick = (time) => {
     setSelectedTime(time.value);
   };
@@ -28,6 +33,7 @@ const RuntimeSelection = () => {
       .post(`${baseURL}/tempo`, { time: selectedTime }) // Envia o tempo ao backend
       .then((response) => {
         console.log("Tempo enviado com sucesso:", response.data);
+        // Após enviar o tempo, navega para a página de classificação
         navigate('/escolha_classificacao');
       })
       .catch((error) => {
@@ -54,37 +60,12 @@ const RuntimeSelection = () => {
           ))}
         </div>
       </div>
-      <button className="button-left" onClick={() => navigate('/escolha_generos')}>
-        <svg viewBox="0 0 24 24" className="arr-2" xmlns="http://www.w3.org/2000/svg">
-          <path d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z" />
-        </svg>
-        <span className="text">Voltar</span>
-        <span className="circle"></span>
-        <svg viewBox="0 0 24 24" className="arr-1" xmlns="http://www.w3.org/2000/svg">
-          <path d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z" />
-        </svg>
-      </button>
 
-      {/* Botão que envia o tempo ao backend antes de navegar */}
-      <button
-        className="button-right"
-        onClick={() => {
-          enviarTempoParaBackend(); // Envia o tempo ao backend
-        }}
-      >
-        <svg viewBox="0 0 24 24" class="arr-2" xmlns="http://www.w3.org/2000/svg">
-          <path
-            d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"
-          ></path>
-        </svg>
-        <span class="text">Prosseguir</span>
-        <span class="circle"></span>
-        <svg viewBox="0 0 24 24" class="arr-1" xmlns="http://www.w3.org/2000/svg">
-          <path
-            d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"
-          ></path>
-        </svg>
-      </button>
+      <BackButton backLink="/escolha_generos" />
+      <NextButton
+        proceedLink="/escolha_classificacao"
+        onProceedClick={enviarTempoParaBackend} // Passando a função de envio de tempo
+      />
     </div>
   );
 };
