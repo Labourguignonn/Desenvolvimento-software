0b7c081cbf7e5991356ea62f436aca6fbe95524f
Luis <luisedu2395@gmail.com>
2025-03-11 18:01:22 -0300

    Finalmente acabou a parte do login e registro, mais a chave criptografada

--- Frontend/app/src/pages/Login/Login.jsx
+++ Frontend/app/src/pages/Login/Login.jsx
@@ -1,20 +1,53 @@
 import { useState } from "react";
+import { useEffect } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import "./Login.css";
 import { baseURL } from "../../services/config";
 import eyeIcon from "../../assets/eye-icon.svg";
 import ErrorMessage from "../../components/ErrorMessage/ErrorMessage";
+import Samurai from "../../assets/Login_register/Samurai.png";
+import Predio from "../../assets/Login_register/Predio.png";
+import Bateria from "../../assets/Login_register/Bateria.png";
+import Praia from "../../assets/Login_register/Cidade_de_deus.png";
+import Kill_bill from "../../assets/Login_register/Kill_bill.png";
+import Fogueira from "../../assets/Login_register/Fogueira.png";
 
 function Login() {
   const navigate = useNavigate();
-  const [user, setUser] = useState({ username: "", password: "", key: "", confirmPassword: "" });
+  const [user, setUser] = useState({ username: "", password: "", confirmPassword: "" });
   const [isRegistering, setIsRegistering] = useState(false);
-  const [showPassword, setShowPassword] = useState({ password: false, confirmPassword: false, key: false });
+  const [showPassword, setShowPassword] = useState({ password: false, confirmPassword: false});
   const [errorMessage, setErrorMessage] = useState("");
+  const [randomImage, setRandomImage] = useState("");
 
-  const toggleVisibility = (field) => setShowPassword((prev) => ({ ...prev, [field]: !prev[field] }));
-  const handleChange = (e) => setUser({ ...user, [e.target.name]: e.target.value });
+  useEffect(() => {
+    const cadastroImages = [
+      { imagem: Samurai },
+      { imagem: Predio },
+      { imagem: Bateria }
+    ];
+
+    const loginImages = [
+      { imagem: Praia },
+      { imagem: Kill_bill },
+      { imagem: Fogueira }
+    ];
+
+    const imageLinks = isRegistering ? cadastroImages : loginImages;
+    const randomIndex = Math.floor(Math.random() * imageLinks.length);
+    const selectedImage = imageLinks[randomIndex];
+    setRandomImage(selectedImage.imagem);
+  }, [isRegistering]);
+
+
+  const handleChange = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+
+  const toggleVisibility = (name) => {
+    setShowPassword({ ...showPassword, [name]: !showPassword[name] });
+  };
   const handleCloseError = () => setErrorMessage("");
 
   const verificarUsuario = async () => {
@@ -42,14 +75,19 @@ function Login() {
   };
 
   const loginUsuario = async () => {
-    if (!user.username || !user.password || !user.key) return setErrorMessage("Preencha todos os campos!");
+    if (!user.username || !user.password) return setErrorMessage("Preencha todos os campos!");
 
     try {
       const authResponse = await axios.post(`${baseURL}/verificar-login`, { username: user.username, password: user.password });
-      if (!authResponse.data.success) return setErrorMessage("Usuário ou senha incorretos!");
-
-      const keyResponse = await axios.post(`${baseURL}/receber_chave`, user);
-      keyResponse.data.chave ? navigate("/filtros") : setErrorMessage("Erro ao validar a chave.");
+      if (!authResponse.data.success) {
+        return setErrorMessage("Usuário ou senha incorretos!");
+      }
+      
+      if (user.username && user.password) {
+        navigate("/filtros");
+      } else {
+        setErrorMessage("Preencha todos os campos corretamente!");
+      }
     } catch {
       setErrorMessage("Erro ao enviar dados.");
     }
@@ -58,32 +96,49 @@ function Login() {
   return (
     <div className="container_login">
       {errorMessage && <ErrorMessage message={errorMessage} onClose={handleCloseError} />}
+      <div className="container_image_film">
+          <img src= {randomImage}/>
+      </div>
       <div className={isRegistering ? "container_register" : "container_informations"}>
-        <h2>{isRegistering ? "Registre-se" : "Login"}</h2>
-        {[{ label: "Usuário", name: "username" }, { label: "Senha", name: "password" }, ...(isRegistering ? [{ label: "Confirmar Senha", name: "confirmPassword" }] : [{ label: "Senha OPEN AI", name: "key" }])].map(({ label, name }) => (
+        <div className={isRegistering ? "title-register" : "title-login"}>
+            {isRegistering ? "Registre-se" : "Login"}
+            <p>
+              {isRegistering ? "Já possui uma conta?" : "Venha descobrir novos filmes!"}
+            {isRegistering && (
+              <span onClick={() => setIsRegistering(false)} className="toggle_form" style={{ cursor: "pointer", marginLeft: "1px" }}>
+                Login
+              </span>
+            )}
+            </p>
+          </div>
+          {[{ label: "Usuário", name: "username", type: "text" }, { label: "Senha", name: "password", type: showPassword.password ? "text" : "password" }, ...(isRegistering ? [{ label: "Confirmar Senha", name: "confirmPassword", type: showPassword.confirmPassword ? "text" : "password" }] : [])].map(({ label, name, type }) => (
           <div className="container_title_input" key={name}>
             <p className="mt-2">{label}</p>
             <div className="field_with_eye">
               <input
-                type={showPassword[name] ? "text" : "password"}
+                type={type}
                 placeholder={`Digite sua ${label.toLowerCase()}...`}
                 name={name}
                 value={user[name]}
                 onChange={handleChange}
                 className="input_box"
-              />
-              {(name !== "username") && (
-                <button type="button" className="eye-icon" onClick={() => toggleVisibility(name)}>
-                  <img src={eyeIcon} alt="Ícone de olho" />
-                </button>
-              )}
+                />
+                {(name !== "username") && (
+                  <button type="button" className="eye-icon" onClick={() => toggleVisibility(name)}>
+                    <img src={eyeIcon} alt="Ícone de olho" />
+                  </button>
+                )}
+              </div>
             </div>
-          </div>
-        ))}
-        <button className="button_config" onClick={isRegistering ? registrarUsuario : loginUsuario}>{isRegistering ? "Registrar" : "Enviar"}</button>
-        <p onClick={() => setIsRegistering(!isRegistering)} className="toggle_form" style={{ cursor: "pointer" }}>
-          {isRegistering ? "Já tem uma conta? Faça login" : "Não tem uma conta? Registre-se"}
-        </p>
+          ))}
+          <button className={isRegistering ? "button_config_register" : "button_config_login"} onClick={isRegistering ? registrarUsuario : loginUsuario}>
+            {isRegistering ? "Registrar" : "Enviar"}
+          </button>
+          {!isRegistering && (
+          <p onClick={() => setIsRegistering(!isRegistering)} className="toggle_form" style={{ cursor: "pointer" }}>
+            Não tem uma conta? Registre-se
+          </p>
+        )}
       </div>
     </div>
   );
