d766b3afc864a74e695783d9edb46598f55562a5
Lucas Araujo Bourguignon <107072168+Labourguignonn@users.noreply.github.com>
2025-03-24 21:29:54 -0300

    scroll and size adjusted

--- Frontend/app/src/services/config.js
+++ Frontend/app/src/services/config.js
@@ -1,6 +1,6 @@
 
 // CHAVE PARA O LOCALHOST
-// export const baseURL = "http://localhost:5000/";
+export const baseURL = "http://localhost:5000/";
 
 // CHAVE PARA O VERCEL
-export const baseURL = "https://desenvolvimento-software-ayeh.vercel.app";
+// export const baseURL = "https://desenvolvimento-software-ayeh.vercel.app";
