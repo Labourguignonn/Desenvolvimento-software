0ffe48fb92ecb109d5f14b339a51fe939bf14942
Lima Araújo <158223826+limaraujo@users.noreply.github.com>
2025-03-27 09:59:13 -0300

    Update Login.jsx

--- Frontend/app/src/pages/Login/Login.jsx
+++ Frontend/app/src/pages/Login/Login.jsx
@@ -14,7 +14,7 @@ import Praia from "../../assets/Login_register/Cidade_de_deus.png";
 import Kill_bill from "../../assets/Login_register/Kill_bill.png";
 import Fogueira from "../../assets/Login_register/Fogueira.png";
 
-function Login() {
+function Login({isLogged, setIsLogged}) {
   const navigate = useNavigate();
   const [user, setUser] = useState({ username: "", password: "", confirmPassword: "" });
   const [isRegistering, setIsRegistering] = useState(false);
@@ -86,6 +86,7 @@ function Login() {
   
       if (user.username && user.password) {
         localStorage.setItem("username", user.username);
+        setIsLogged(true);
         navigate("/filtros");
       } else {
         setErrorMessage("Preencha todos os campos corretamente!");
